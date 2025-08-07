0e345347ad9c70a812c4a5d3f90ab165d52b4396
AleeHz <alee.hernandez.oso@gmail.com>
2025-08-07 13:26:39 -0300

    ...

---
+++ mi_proyecto_ale/inicio/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+<<<<<<< HEAD
+from .models import Post, Categoria
+
+admin.site.register(Post)
+admin.site.register(Categoria)
+=======
+# Register your models here.
+>>>>>>> 4c872381eb5f2ff4499f79352ecfad2e8ad62839
